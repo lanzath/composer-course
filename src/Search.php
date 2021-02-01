2e992bee97cb2283d4ea088d5e6394e029008042
Thiago Lanza <lanzathm@gmail.com>
2021-02-01 18:12:18 -0300

    Class to search an address from zipcode

---
+++ src/Search.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Lanza\DigitalCep;
+
+class Search
+{
+  private $url = 'https://viacep.com.br/ws/';
+
+  /**
+   * Pega o endereço a partir de um cep informado.
+   *
+   * @param  string $zipcode
+   * @return array
+   */
+  public function getAddressFromZipcode(string $zipCode): array
+  {
+    // Tudo que não for número vai ser removido.
+    $zipCode = preg_replace('/[^0-9]im/', '', $zipCode);
+
+    $get = file_get_contents($this->url . $zipCode . "/json");
+
+    return (array) json_decode($get);
+  }
+}
