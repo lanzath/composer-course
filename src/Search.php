281a0787cf8d3a79c0d734e196838903864eb75f
Thiago Lanza <lanzathm@gmail.com>
2021-02-10 16:16:07 -0300

    Indent adjust

--- src/Search.php
+++ src/Search.php
@@ -4,21 +4,21 @@ namespace Lanza\DigitalCep;
 
 class Search
 {
-  private $url = 'https://viacep.com.br/ws/';
+    private $url = 'https://viacep.com.br/ws/';
 
-  /**
-   * Pega o endereço a partir de um cep informado.
-   *
-   * @param  string $zipcode
-   * @return array
-   */
-  public function getAddressFromZipcode(string $zipCode): array
-  {
-    // Tudo que não for número vai ser removido.
-    $zipCode = preg_replace('/[^0-9]im/', '', $zipCode);
+    /**
+     * Pega o endereço a partir de um cep informado.
+     *
+     * @param  string $zipcode
+     * @return array
+     */
+    public function getAddressFromZipcode(string $zipCode): array
+    {
+        // Tudo que não for número vai ser removido.
+        $zipCode = preg_replace('/[^0-9]im/', '', $zipCode);
 
-    $get = file_get_contents($this->url . $zipCode . "/json");
+        $get = file_get_contents($this->url . $zipCode . "/json");
 
-    return (array) json_decode($get);
-  }
+        return (array) json_decode($get);
+    }
 }
